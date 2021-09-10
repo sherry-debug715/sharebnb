026551d011ffeb248bc0e227093942e31ababc24
sherryyu <bambooyu715@gmail.com>
2021-09-10 10:03:07 -0400

    finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,3 +5,38 @@ module.exports = router;
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+// Testing setTokenCookie middleware from utils/auth.js
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+// Testing restoreUser middleware from utils/auth.js
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+// Testing requireAuth middleware from utils/auth.js: If there is no session user, the route will return an error. Otherwise it will return the session user's information.
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
