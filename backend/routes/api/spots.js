e6fc255d008c682148cd15c92351fad7ac42751a
sherryyu <bambooyu715@gmail.com>
2021-09-14 13:44:45 -0400

    POST and Patch problems

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,10 +4,59 @@ const asyncHandler = require('express-async-handler');
 const {Spot, Image} = require('../../db/models')
 const router = express.Router();
 
-router.get('/spots',  asyncHandler(async function(req, res) {
+// show a list of all spots
+router.get('/',  asyncHandler(async(req, res) => {
     const spots = await Spot.findAll({
         include: Image
     });
     // console.log(spots);
     res.json(spots);
   }));
+
+//see the details of a spot
+router.get('/:spotId', asyncHandler(async(req, res) => {
+const id = req.params.spotId;
+const spotInfo = await Spot.findByPk(id)
+res.json(spotInfo);
+}));
+
+//user can add an spot
+router.post('/:userId/spots', asyncHandler(async(req, res) => {
+    const { userId, city, state, name, price} = req.body;
+    await Spot.create({
+        userId,
+        city,
+        state,
+        name,
+        price
+    })
+    res.redirect(`/spots/${spotId}`);
+}));
+
+// user can edit an spot
+router.patch('/:userId/spots/:spotId/edit', asyncHandler(async(req, res) => {
+    const id = req.body.id;
+    await Spot.update(
+        
+    )
+    // const { userId, city, state, name, price} = req.body;
+    // await Spot.update({
+    //     userId,
+    //     city,
+    //     state,
+    //     name,
+    //     price
+    // })
+    res.redirect(`/spots/${spotId}`);
+}));
+
+//Delete an spot
+router.delete("/:userId/spots/:spotId/delete", asyncHandler(async function (req, res) {
+    const spot = await Spot.findByPk(req.params.id);
+    if(!spot) throw new Error('Cannot find item');
+    const spotId = spot.id;
+    await Spot.destroy({ where: { id: spotId}});
+    res.redirect(`/`);
+}));
+
+module.exports = router;
